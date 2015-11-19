f449e5d09f37d3085527f33255d0ff03e1e881cb
Codeklopper <basvvliet@gmail.com>
2015-11-19 09:53:27 +0100

    changed default names

--- app/Config.js
+++ app/Config.js
@@ -5,7 +5,7 @@ var wijnConfig = new ConfigItem('wijn', 'img/bg_wijn.jpg', ['Monique'], false);
 
 
 var configurations = [];
-configurations['koffie'] 	= new ConfigItem('koffie', 'img/bg_koffie.jpg', ['Bas', 'Monique', 'Benjamin', 'Leo'], 'audio/koffie.mp3');
+configurations['koffie'] 	= new ConfigItem('koffie', 'img/bg_koffie.jpg', ['Bas', 'Monique', 'Benjamin', 'Onno', 'Lukasz'], 'audio/koffie.mp3');
 configurations['bier']   	= new ConfigItem('bier', 'img/bg_bier.jpg', ['Bas'], false);
 configurations['tequila']	= new ConfigItem('tequila', 'img/bg_tequila.jpg', ['Bas', 'Patrick', 'Leo', 'Benjamin'], false);
 configurations['wijn']   	= new ConfigItem('wijn', 'img/bg_wijn.jpg', ['Monique'], false);
